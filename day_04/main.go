cafff8f014232e43a34e8377ca9ce99743a4cb3d
harphield <harphield@gmail.com>
2025-12-04 21:17:45 +0100

    Day 4 part 1

---
+++ day_04/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/harphield/advent2025/inputreader"
+)
+
+func main() {
+	rows := inputreader.ReadInputFile()
+
+	var grid []bool
+	width := 0
+	height := 0
+
+	for _, row := range rows {
+		if width == 0 {
+			width = len(row)
+		}
+
+		for _, c := range row {
+			if c == '.' {
+				grid = append(grid, false)
+			} else {
+				grid = append(grid, true)
+			}
+		}
+
+		height++
+	}
+
+	draw_grid(grid, width)
+
+	can_be_picked_up := 0
+
+	for i, v := range grid {
+		if !v {
+			continue
+		}
+
+		x := i % width
+		y := int(math.Floor(float64(i / width)))
+
+		rolls_around := 0
+
+		// left
+		if x > 0 && grid[coords_to_index(x-1, y, width)] == true {
+			rolls_around++
+		}
+		// right
+		if x < width-1 && grid[coords_to_index(x+1, y, width)] == true {
+			rolls_around++
+		}
+		// up
+		if y > 0 && grid[coords_to_index(x, y-1, width)] == true {
+			rolls_around++
+		}
+		// down
+		if y < height-1 && grid[coords_to_index(x, y+1, width)] == true {
+			rolls_around++
+		}
+		// up left
+		if x > 0 && y > 0 && grid[coords_to_index(x-1, y-1, width)] == true {
+			rolls_around++
+		}
+		// up right
+		if x < width-1 && y > 0 && grid[coords_to_index(x+1, y-1, width)] == true {
+			rolls_around++
+		}
+		// down left
+		if x > 0 && y < height-1 && grid[coords_to_index(x-1, y+1, width)] == true {
+			rolls_around++
+		}
+		// down right
+		if x < width-1 && y < height-1 && grid[coords_to_index(x+1, y+1, width)] == true {
+			rolls_around++
+		}
+
+		if rolls_around < 4 {
+			//fmt.Println(x, y, " can be picked up")
+			can_be_picked_up++
+		}
+	}
+
+	fmt.Println("RESULT PART 1: ", can_be_picked_up)
+}
+
+func coords_to_index(x int, y int, width int) int {
+	return (y * width) + x
+}
+
+func draw_grid(grid []bool, width int) {
+	for i, v := range grid {
+		if i%width == 0 {
+			fmt.Println()
+		}
+
+		if v {
+			fmt.Print("@")
+		} else {
+			fmt.Print(".")
+		}
+	}
+
+	fmt.Println()
+}
