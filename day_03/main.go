12af3481fff345dfc7e38c04ef6b697061430f58
harphield <harphield@gmail.com>
2025-12-04 09:27:08 +0100

    Day 3 part 2

--- day_03/main.go
+++ day_03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 
 	"github.com/harphield/advent2025/inputreader"
@@ -11,30 +12,54 @@ func main() {
 	rows := inputreader.ReadInputFile()
 
 	sum := 0
+	sum2 := 0
 
 	for _, row := range rows {
-		first := 0
-		second := 0
-		length := len(row)
-
-		for i, c := range row {
-			joltage, err := strconv.ParseInt(string(c), 10, 64)
-
-			if err != nil {
-				panic("parse int failed")
-			}
-
-			if i != length-1 && int(joltage) > first {
-				first = int(joltage)
-				second = 0
-			} else if int(joltage) > second {
-				second = int(joltage)
-			}
-		}
-
-		fmt.Println(first, second)
-		sum += first*10 + second
+		sum += solve(row, 2)
+		sum2 += solve(row, 12)
 	}
 
 	fmt.Println("RESULT PART 1: ", sum)
+	fmt.Println("RESULT PART 2: ", sum2)
+}
+
+func solve(row string, bat_length int) int {
+	values := make([]int, bat_length)
+	length := len(row)
+
+	// we can search for values from this index
+	search_from := 0
+
+	for i, c := range row {
+		joltage, err := strconv.ParseInt(string(c), 10, 64)
+
+		if err != nil {
+			panic("parse int failed")
+		}
+
+		if length-bat_length-i < 0 {
+			search_from = -(length - bat_length - i)
+		}
+
+		reset_mode := false
+		for vi := search_from; vi < len(values); vi++ {
+			if reset_mode {
+				values[vi] = 0
+				continue
+			}
+
+			if int(joltage) > values[vi] {
+				values[vi] = int(joltage)
+				reset_mode = true
+			}
+		}
+	}
+
+	result := 0
+
+	for i, value := range values {
+		result += value * int(math.Pow10(bat_length-i-1))
+	}
+
+	return result
 }
