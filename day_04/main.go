db67e98b7f01b2bcd62bf17d8fb14a0b798c5e50
harphield <harphield@gmail.com>
2025-12-04 21:30:08 +0100

    Day 4 part 2

--- day_04/main.go
+++ day_04/main.go
@@ -33,57 +33,79 @@ func main() {
 	draw_grid(grid, width)
 
 	can_be_picked_up := 0
+	loops := 0
+	var to_remove []int
 
-	for i, v := range grid {
-		if !v {
+	for {
+		for i, v := range grid {
+			if !v {
+				continue
+			}
+
+			x := i % width
+			y := int(math.Floor(float64(i / width)))
+
+			rolls_around := 0
+
+			// left
+			if x > 0 && grid[coords_to_index(x-1, y, width)] == true {
+				rolls_around++
+			}
+			// right
+			if x < width-1 && grid[coords_to_index(x+1, y, width)] == true {
+				rolls_around++
+			}
+			// up
+			if y > 0 && grid[coords_to_index(x, y-1, width)] == true {
+				rolls_around++
+			}
+			// down
+			if y < height-1 && grid[coords_to_index(x, y+1, width)] == true {
+				rolls_around++
+			}
+			// up left
+			if x > 0 && y > 0 && grid[coords_to_index(x-1, y-1, width)] == true {
+				rolls_around++
+			}
+			// up right
+			if x < width-1 && y > 0 && grid[coords_to_index(x+1, y-1, width)] == true {
+				rolls_around++
+			}
+			// down left
+			if x > 0 && y < height-1 && grid[coords_to_index(x-1, y+1, width)] == true {
+				rolls_around++
+			}
+			// down right
+			if x < width-1 && y < height-1 && grid[coords_to_index(x+1, y+1, width)] == true {
+				rolls_around++
+			}
+
+			if rolls_around < 4 {
+				//fmt.Println(x, y, " can be picked up")
+				can_be_picked_up++
+				to_remove = append(to_remove, i)
+			}
+		}
+
+		if loops == 0 {
+			fmt.Println("RESULT PART 1: ", can_be_picked_up)
+		}
+
+		loops++
+
+		if len(to_remove) > 0 {
+			for _, index := range to_remove {
+				grid[index] = false
+			}
+
+			to_remove = make([]int, 0)
 			continue
 		}
 
-		x := i % width
-		y := int(math.Floor(float64(i / width)))
-
-		rolls_around := 0
-
-		// left
-		if x > 0 && grid[coords_to_index(x-1, y, width)] == true {
-			rolls_around++
-		}
-		// right
-		if x < width-1 && grid[coords_to_index(x+1, y, width)] == true {
-			rolls_around++
-		}
-		// up
-		if y > 0 && grid[coords_to_index(x, y-1, width)] == true {
-			rolls_around++
-		}
-		// down
-		if y < height-1 && grid[coords_to_index(x, y+1, width)] == true {
-			rolls_around++
-		}
-		// up left
-		if x > 0 && y > 0 && grid[coords_to_index(x-1, y-1, width)] == true {
-			rolls_around++
-		}
-		// up right
-		if x < width-1 && y > 0 && grid[coords_to_index(x+1, y-1, width)] == true {
-			rolls_around++
-		}
-		// down left
-		if x > 0 && y < height-1 && grid[coords_to_index(x-1, y+1, width)] == true {
-			rolls_around++
-		}
-		// down right
-		if x < width-1 && y < height-1 && grid[coords_to_index(x+1, y+1, width)] == true {
-			rolls_around++
-		}
-
-		if rolls_around < 4 {
-			//fmt.Println(x, y, " can be picked up")
-			can_be_picked_up++
-		}
+		break
 	}
 
-	fmt.Println("RESULT PART 1: ", can_be_picked_up)
+	fmt.Println("RESULT PART 2: ", can_be_picked_up)
 }
 
 func coords_to_index(x int, y int, width int) int {
