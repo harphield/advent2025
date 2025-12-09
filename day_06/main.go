5636385d332a31ad1f4220746fe0c022fae6c400
harphield <harphield@gmail.com>
2025-12-09 21:57:57 +0100

    Day 6 part 2

--- day_06/main.go
+++ day_06/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 
@@ -15,6 +16,7 @@ func main() {
 	rows := inputreader.ReadInputFile()
 
 	var columns [][]int
+	var part2_columns [][]string
 	var operators []string
 
 	for _, row := range rows {
@@ -28,6 +30,12 @@ func main() {
 				}
 			}
 
+			if len(part2_columns) == 0 {
+				for range len(row) {
+					part2_columns = append(part2_columns, make([]string, 0))
+				}
+			}
+
 			for i, n := range number_matches {
 				number, err := strconv.ParseInt(n, 10, 64)
 
@@ -37,15 +45,21 @@ func main() {
 
 				columns[i] = append(columns[i], int(number))
 			}
+
+			// in part 2 I fill all characters in the row
+			for i2, s := range row {
+				if i2 >= len(part2_columns) {
+					part2_columns = append(part2_columns, make([]string, 0))
+				}
+				part2_columns[i2] = append(part2_columns[i2], string(s))
+			}
 		} else {
 			// this only appears once in the end
 			operators = re_symbols.FindAllString(row, -1)
 		}
 	}
 
-	fmt.Println(operators)
-	fmt.Println(columns)
-
+	// part 1
 	sum := 0
 	for i, o := range operators {
 		if o == "+" {
@@ -62,5 +76,62 @@ func main() {
 		}
 	}
 
+	// part 2
+	current_operator_index := len(operators) - 1
+	column_index := len(part2_columns) - 1
+	sum2 := 0
+	multi_value := 1
+	for {
+		number := build_number_from_column(part2_columns[column_index])
+
+		if number == 0 {
+			if current_operator_index-1 < 0 {
+				break
+			}
+
+			if operators[current_operator_index] == "*" {
+				sum2 += multi_value
+				multi_value = 1
+			}
+
+			current_operator_index--
+			column_index--
+			continue
+		}
+
+		if operators[current_operator_index] == "+" {
+			sum2 += number
+		} else {
+			multi_value *= number
+		}
+
+		if column_index-1 < 0 {
+			if operators[current_operator_index] == "*" {
+				sum2 += multi_value
+			}
+			break
+		}
+
+		column_index--
+	}
+
 	fmt.Println("RESULT PART 1: ", sum)
+	fmt.Println("RESULT PART 2: ", sum2)
+}
+
+func build_number_from_column(column []string) int {
+	result := 0
+	decimals := 0
+	for i := len(column) - 1; i >= 0; i-- {
+		number, err := strconv.ParseInt(column[i], 10, 64)
+
+		if err != nil {
+			continue
+		}
+
+		result += int(number * int64(math.Pow10(decimals)))
+		decimals++
+	}
+
+	return result
 }
