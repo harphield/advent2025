6bb663d21566446dcb939bbce857bab21d465b92
harphield <harphield@gmail.com>
2025-12-11 22:10:03 +0100

    Day 7 part 1

---
+++ day_07/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/harphield/advent2025/inputreader"
+)
+
+type splitter struct {
+	x int
+	y int
+}
+
+const EMPTY = 0
+const START = 1
+const SPLITTER = 2
+const BEAM = 3
+
+func main() {
+	rows := inputreader.ReadInputFile()
+
+	x := 0
+	y := 0
+
+	var hitmap [][]int
+
+	width := 0
+
+	splitters := 0
+
+	for _, row := range rows {
+		if width == 0 {
+			width = len(row)
+		}
+
+		hitmap = append(hitmap, make([]int, 0))
+
+		for _, c := range row {
+			switch c {
+			case 'S':
+				// start
+				hitmap[y] = append(hitmap[y], START)
+			case '^':
+				// splitter
+				hitmap[y] = append(hitmap[y], SPLITTER)
+				// check if a ray is coming in from above
+				if hitmap[y-1][x] == BEAM {
+					if x > 0 {
+						hitmap[y][x-1] = BEAM
+					}
+
+					splitters++
+				}
+			default:
+				if y > 0 && (hitmap[y-1][x] == BEAM || hitmap[y-1][x] == START) {
+					hitmap[y] = append(hitmap[y], BEAM)
+				} else if x > 0 && hitmap[y][x-1] == SPLITTER {
+					hitmap[y] = append(hitmap[y], BEAM)
+				} else {
+					hitmap[y] = append(hitmap[y], EMPTY)
+				}
+			}
+
+			x++
+		}
+
+		y++
+		x = 0
+	}
+
+	fmt.Println(hitmap)
+
+	fmt.Println("RESULT PART 1: ", splitters)
+}
